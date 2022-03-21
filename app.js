0068ce4a21e709eef824923e62d67d6cc7b1375a
jkjh8 <jkjh82@naver.com>
2022-03-21 23:49:42 +0900

    Make HTTP and HTTPS Server

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+app.use(
+  cors({
+    origin: ['https://locahost:3443', 'http://localhost:3000'],
+    credentials: true
+  })
+)
 const router = require('./routes/index')
 
-app.get('*', (req, res, next) => {
-  console.log('req.secure == ' + req.secure)
-
-  if (req.secure) {
-    // --- https
-    next()
-  } else {
-    // -- http
-    let to = 'https://' + req.headers.host + req.url
-    console.log('to ==> ' + to)
-
-    return res.redirect('https://' + req.headers.host + req.url)
-  }
-})
+// redirect to HTTPS
+// app.get('*', (req, res, next) => {
+//   console.log('req.secure == ' + req.secure)
+//   if (req.secure) {
+//     next()
+//   } else {
+//     return res.redirect('https://' + req.headers.host + req.url)
+//   }
+// })
 
 app.use('/', router)
 
--- bin/www.js
+++ bin/www.js
@@ -1,31 +1,28 @@
 const logger = require('../config/logger')
-
-const path = require('path')
 const fs = require('fs')
 const app = require('../app')
 const https = require('https')
 const http = require('http')
-const port = process.env.PORT || 443
 
-const options = {
-  key: fs.readFileSync('ssl/server.key'),
-  cert: fs.readFileSync('ssl/server.pem')
-}
-const httpsServer = https.createServer(options, app)
-const httpServer = http.createServer(app)
+// make HTTPS Server
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.pem')
+  },
+  app
+)
 
 httpsServer.on('error', (error) => {
   if (error.syscall !== 'listen') {
     throw error
   }
-  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
-  // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      logger.error(bind + ' requires elevated privileges')
+      logger.error(3443 + ' requires elevated privileges')
       process.exit(1)
     case 'EADDRINUSE':
-      logger.error(bind + ' is already in use')
+      logger.error(3443 + ' is already in use')
       process.exit(1)
     default:
       throw error
@@ -33,10 +30,31 @@ httpsServer.on('error', (error) => {
 })
 
 httpsServer.on('listening', () => {
-  const addr = httpsServer.address()
-  let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
-  logger.info('Listening on ' + bind)
+  logger.info('Listening HTTPS on ' + 3443)
 })
 
-httpsServer.listen(port)
-httpServer.listen(80)
+httpsServer.listen(3443)
+
+//make http Server
+const httpServer = http.createServer(app)
+httpsServer.on('error', (error) => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+  switch (error.code) {
+    case 'EACCES':
+      logger.error(3000 + ' requires elevated privileges')
+      process.exit(1)
+    case 'EADDRINUSE':
+      logger.error(3000 + ' is already in use')
+      process.exit(1)
+    default:
+      throw error
+  }
+})
+
+httpServer.on('listening', () => {
+  logger.info('Listening HTTP on ' + 3000)
+})
+
+httpServer.listen(3000)
